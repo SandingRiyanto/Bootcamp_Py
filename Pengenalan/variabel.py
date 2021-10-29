387a7ec79dacfde6af67041f2f39782713472d90
SandingRiyanto <riyanto.sanding.13@gmail.com>
2021-10-29 10:08:39 +0700

    aturan penulisan variabel pada python

---
+++ Pengenalan/variabel.py
@@ -0,0 +1,17 @@
+# inisiasi satu-satu
+a = 7
+b = 2
+c = 10
+
+# inisiasi banyak
+e, f, g = 12, 62, 11
+
+# inisiasi variable bernilai sama
+h = i = j = 33
+
+# cetak variabel
+print("nilai a:", a)
+
+# menghapus variabel dg fungsi : del()
+del(b)
+# print(b) # akan error
